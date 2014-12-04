d8c6ff7796290be00fc48838eef2ffb2e9d457ea
Teresa Fikes <Ardenent1@gmail.com>
2014-12-03 20:07:09 -0600

    Practicing Switch
    Practicing the switch in the JavaScript

--- WK2/goal2_practice-3/js/main.js
+++ WK2/goal2_practice-3/js/main.js
@@ -363,6 +363,10 @@ var fruit = "Oranges";
 		case "Cherries":
 			console.log("Cherries are $3.00 a pound.");
 			break;
+		case "Mangoes":
+		case "Papayas";
+			console.log("Mangoes and papayas are $2.79 a pound");
+			break;
 		default;
 			console.log("Sorry, we are out of " + fruit + ".");
 	};
